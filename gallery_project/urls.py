888377684e7d28457840344ba72258278abc3a40
AugustineOchieng <gusochieng@gmail.com>
2019-05-12 21:43:20 +0300

    Create url path to views

--- gallery_project/urls.py
+++ gallery_project/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^gallery/',include('gallery.urls'))
+    url(r'^', include('gallery.urls'))
+    
 ]
