89befc619814d8571bf0af748e0530611a0a032a
AugustineOchieng <gusochieng@gmail.com>
2019-05-12 19:56:32 +0300

    Imported redirect model

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$', views.home, name='gallery-home'),
+    url('^lensflare/', views.lensflare, name='gallery-lensflare'),
+    url('^photoboom/',views.photoboom,name = 'gallery-photoboom')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
