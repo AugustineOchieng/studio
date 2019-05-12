03b2a367261af77fb44f16c13ca0b8957e2dfb88
AugustineOchieng <gusochieng@gmail.com>
2019-05-12 19:57:20 +0300

    Create classes and defined routes to respective pages

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Image, Picture
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the my Gallery')
+def home(request):
+    context = {
+      'images': Image.objects.all(),
+      
+    }
+    
+    return render(request,'gallery.html', context)
+
+def photoboom(request):
+    return render(request, 'photoboom.html')
+    
+
+def lensflare(request):
+    
+    context = {
+      'pictures': Picture.objects.all()
+      
+    }
+    
+    return render(request, 'lensflare.html', context)
