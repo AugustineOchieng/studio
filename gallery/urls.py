262f37e5f9e17232185e338b7e6db100598f9865
AugustineOchieng <gusochieng@gmail.com>
2019-05-14 10:21:08 +0300

    Add routing to location.html

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,7 +8,8 @@ urlpatterns=[
     url('^$', views.home, name='gallery-home'),
     url('^lensflare/', views.lensflare, name='gallery-lensflare'),
     url('^photoboom/', views.photoboom, name='gallery-photoboom'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(\d+)',views.location,name='location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
